3c21ee9992ef468593d35d1098d2821c67bc3a43
ehab <Melhemehab@gmail.com>
2021-02-01 22:40:04 +0200

    update

--- .history/server_20210201223916.js
+++ .history/server_20210201223916.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const usersRouter = require("./Routers/user");
 const app = express();
-app.use(express.static("ibraheemKittani_ehabMelhem/build"));
+app.use()
 
 app.use("/users", usersRouter);
 
